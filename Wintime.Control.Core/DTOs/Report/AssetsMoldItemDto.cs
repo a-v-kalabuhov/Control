20d7bdfde6f265520bbe8ce3da5ed54b6c8989a6
Alex Kalabukhov <a.v.kalabuhov@gmail.com>
2026-06-01 13:00:41 +0300

    Улучшены отчёты

--- Wintime.Control.Core/DTOs/Report/AssetsMoldItemDto.cs
+++ Wintime.Control.Core/DTOs/Report/AssetsMoldItemDto.cs
@@ -6,5 +6,9 @@ public class AssetsMoldItemDto
     public string? MoldName { get; set; }
     public int TotalCycles { get; set; }
     public decimal WorkHours { get; set; }
+    public int MaxResourceCycles { get; set; }
+    public int? To1Cycles { get; set; }
+    public int? To2Cycles { get; set; }
+    public int AllTimeTotalCycles { get; set; }
     public int RemainingResource { get; set; }
 }
